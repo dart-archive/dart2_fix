ae7b24b4cbad08832793165642d3285d965d6e0c
Devon Carew <devoncarew@google.com>
2018-03-27 13:09:25 -0700

    initial cli ui

--- lib/src/dart2_fix.dart
+++ lib/src/dart2_fix.dart
@@ -3,17 +3,70 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 
-//import 'package:args/args.dart';
+import 'package:args/args.dart';
+
+// Duration.ZERO ==> Duration.zero
+
+// checking/updating sdsdf...
+//
+// main.dart
+//   7 changes Duration.ZERO ==> Duration.zero
+// foo.dart
+//   1 change Duration.BAZ ==> Duration.baz
+//   3 changes Duration.ZERO ==> Duration.zero
+//
+//  37 changes total.
 
 Future<ExitResult> main(List<String> args) async {
-  // TODO: implement the UI
+  ArgParser argParser = new ArgParser();
+  argParser.addFlag('help',
+      negatable: false, abbr: 'h', help: 'Display usage help.');
+  argParser.addFlag('apply',
+      abbr: 'w',
+      negatable: false,
+      help: "Apply the refactoring changes to the project's source files.");
 
-  if (args.isNotEmpty) {
-    return new ExitResult(1, 'unexpected argument: ${args.first}');
+  ArgResults results;
+
+  try {
+    results = argParser.parse(args);
+
+    if (results['help']) {
+      _printUsage(argParser);
+      return ExitResult.ok;
+    }
+  } on FormatException catch (e) {
+    stderr.writeln(e.message);
+    print('');
+    _printUsage(argParser);
+    return new ExitResult(1);
   }
 
+  List<Directory> dirs = results.rest.isEmpty
+      ? [Directory.current]
+      : results.rest.map((s) => new Directory(s)).toList();
+
+  for (Directory dir in dirs) {
+    if (!dir.existsSync()) {
+      return new ExitResult(
+          1, "'${dir.path}' does not exist or is not a directory.");
+    }
+  }
+
+  bool dryRun = !results['apply'];
+
+  // TODO: implement
+  print('dryrun: $dryRun over ${dirs}');
+
   return ExitResult.ok;
+}
+
+void _printUsage(ArgParser argParser) {
+  print('usage: dart2_fix [options...] <directories>');
+  print('');
+  print(argParser.usage);
 }
 
 class ExitResult {
